e31e66950395bc3133033a1148a39f7421886ab7
Null Builds <12244473+nullbuilds@users.noreply.github.com>
2026-05-19 00:17:18 -0500

    feat: add memoizer
    Adds a basic memoization class for caching values.

---
+++ addons/proto_jam/utils/scripts/memoizer.gd
@@ -0,0 +1,37 @@
+class_name Memoizer
+extends RefCounted
+## Caches and automatically refreshes any type when it becomes invalid.
+
+var _value: Variant = null
+var _provider: Callable = Callable()
+var _validator: Callable = Callable()
+
+## Constructs a new memoizer.
+## 
+## The given [param provider] will be called to obtain a new value the first
+## time [method get_value] is invoked or on subsequent calls when the cached
+## value becomes invalid. The provider must take no arguments and return a value
+## to cache.
+## [br][br]
+## A custom [param validator] may be provided to perform additional checks
+## against a cached value. The validator must accept a single parameter of the
+## same type returned by [param provider] and return [code]true[/code] if the
+## instance is valid. The validator will not be during the same request in which
+## a new value is provided as it is assumed providers will only return valid
+## instances.
+func _init(provider: Callable, validator: Callable = Callable()) -> void:
+	_provider = provider
+	_validator = validator
+
+
+## Returns the cached value or obtains a new value if the cached instance is
+## invalid.
+func get_value() -> Variant:
+	if _value is Object and not is_instance_valid(_value):
+		_value = _provider.call()
+	elif null == _value:
+		_value = _provider.call()
+	elif _validator.is_valid() and not _validator.call(_value):
+		_value = _provider.call()
+	
+	return _value
